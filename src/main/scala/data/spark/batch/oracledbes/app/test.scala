9d4c936a6d987c88ec4231d109ff960d8fca53a0
ranzechen <ranzechen@ybs.com>
2017-08-30 10:50:03 +0800

    update

--- src/main/scala/data/spark/batch/oracledbes/app/test.scala
+++ src/main/scala/data/spark/batch/oracledbes/app/test.scala
@@ -1,20 +1,10 @@
 package data.spark.batch.oracledbes.app
 
-import java.io.UnsupportedEncodingException
-import java.util.regex.Pattern
-
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.SparkConf
-import sun.misc.BASE64Encoder
-
-import scala.util.control.Breaks
-import scala.util.matching.Regex
-
 /**
   * Created by ranzechen on 2017/7/21.
   */
 object test {
   def main(args: Array[String]): Unit = {
-    println("hello world")
+      println("hello world")
   }
 }
