1e8e3dd7fecba763b6193d2e51f068c8791aca30
ranzechen <ranzechen@ybs.com>
2017-09-07 14:24:46 +0800

    update

--- src/main/scala/data/spark/batch/test/SparkTest.scala
+++ src/main/scala/data/spark/batch/test/SparkTest.scala
@@ -5,9 +5,9 @@ import org.apache.spark.{SparkConf, SparkContext}
 /**
   * Created by ranzechen on 2017/9/6.
   */
-object test {
+object SparkTest {
   def main(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf().setAppName("test")
+    val sparkConf = new SparkConf().setAppName("SparkTest")
     val sparkContext = new SparkContext(sparkConf)
   }
 }
