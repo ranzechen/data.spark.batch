5234f2fe23fb3b8f41c070a27a748182349fe9e7
ranzechen <ranzechen@ybs.com>
2017-09-07 11:22:15 +0800

    update

--- src/main/scala/data/spark/batch/test/test.scala
+++ src/main/scala/data/spark/batch/test/test.scala
@@ -1,10 +1,13 @@
 package data.spark.batch.test
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by ranzechen on 2017/9/6.
   */
 object test {
   def main(args: Array[String]): Unit = {
-    val a = for(i <- Array("1","2","3","4")) yield i.toInt * 3
+    val sparkConf = new SparkConf().setAppName("test")
+    val sparkContext = new SparkContext(sparkConf)
   }
 }
