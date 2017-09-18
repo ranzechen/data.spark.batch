903e693999f7a7ad9521aec7c2907640f5d0879d
ranzechen <ranzechen@ybs.com>
2017-09-18 19:13:28 +0800

    update

--- src/main/scala/data/spark/batch/memo1parse/app/unionResMemo1.scala
+++ src/main/scala/data/spark/batch/memo1parse/app/unionResMemo1.scala
@@ -10,6 +10,7 @@ import scala.util.parsing.json.JSON
 
 /**
   * Created by ranzechen on 2017/9/16.
+  * 清算数据中第十二个字段为发卡行代码不用再查卡bin表了
   */
 object unionResMemo1 {
   def main(args: Array[String]): Unit = {
