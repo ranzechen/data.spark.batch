5d44b4fbc8e5638eb7b326114c06f146c5ca1766
ranzechen <ranzechen@ybs.com>
2017-09-19 18:21:26 +0800

    update

--- src/main/scala/data/spark/batch/memo1parse/app/card_license.scala
+++ src/main/scala/data/spark/batch/memo1parse/app/card_license.scala
@@ -12,42 +12,50 @@ import scala.util.parsing.json.JSON
   * Created by ranzechen on 2017/9/19.
   * 卡 + 证
   */
-object card_license extends App {
-  val sparkConf = new SparkConf().setAppName("card_license")
-  val sparkContext = new SparkContext(sparkConf)
+object card_license{
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("card_license")
+    val sparkContext = new SparkContext(sparkConf)
 
-  val config = Source.fromFile(args(0)).mkString
-  val raltpath = JSON.parseFull(config).asInstanceOf[Option[Map[String, Any]]].get("raltpath").toString
-  val ralt_choose_itempath = JSON.parseFull(config).asInstanceOf[Option[Map[String, Any]]].get("ralt_choose_itempath").toString
-  val ppbankdspath = JSON.parseFull(config).asInstanceOf[Option[Map[String, Any]]].get("ppbankdspath").toString
+    val config = Source.fromFile(args(0)).mkString
+    val raltpath = JSON.parseFull(config).asInstanceOf[Option[Map[String, Any]]].get("raltpath").toString
+    val ralt_choose_itempath = JSON.parseFull(config).asInstanceOf[Option[Map[String, Any]]].get("ralt_choose_itempath").toString
+    val ppbankdspath = JSON.parseFull(config).asInstanceOf[Option[Map[String, Any]]].get("ppbankdspath").toString
 
-  val ralt = raltutil.getSearcher(raltpath, ralt_choose_itempath, ppbankdspath)
-  sparkContext.textFile(args(1)).map(_.split("#"))
-    .filter(_.length == 7)
-    .filter(15 <= _ (3).length)
-    .map(arr => {
-      val p_t = new Regex("([\\u4e00-\\u9fa5]+)")
-      val cardno = arr(1)
-      val license = arr(3)
-      val phone = arr(4)
-      val name = p_t.findAllIn(arr(2).trim).mkString
-      val raltinfo = ralt.search(cardno)
-      val altbank = raltinfo.get.the_card_segment
-      val bankcode = raltinfo.get.bank_code
-      val cardtype = raltinfo.get.card_type
-      val cardlen = raltinfo.get.card_len
-      s"[${String.format("%1$-12s", altbank)}|${String.format("%1$-8s", bankcode)}|${String.format("%1$-10s", cardtype)}|${String.format("%1$-12s", cardlen)}]:${String.format("%1$-20s", cardno)}#${name + (" " * (20 - name.length * 2))}#${String.format("%1$-15s", if(phone.length == 11) phone else "")}#${String.format("%1$-20s", license)}#${String.format("%1$-8s", bankcode)}#${String.format("%1$-10s", cardtype)}"
-    })
-    .groupBy(_.split("\\|")(0).replaceAll("\\[", "").trim)
-    .map(data => {
-      val alt_bank = data._1
-      val info = data._2.toArray.sortWith(_ > _).take(5)
-      (alt_bank, info)
-    })
-    .sortBy(_._1)
-    .map(data => {
-      data._2.mkString("\n")
-    })
-    .coalesce(10, true)
-    .saveAsTextFile(args(2))
+    val ralt = raltutil.getSearcher(raltpath, ralt_choose_itempath, ppbankdspath)
+    sparkContext.textFile(args(1)).map(_.split("#"))
+      .filter(_.length == 7)
+      .filter(_(1).length != 0)
+      .filter(15 <= _ (3).length)
+      .map(arr => {
+        val p_t = new Regex("([\\u4e00-\\u9fa5]+)")
+        val cardno = arr(1)
+        val license = arr(3)
+        val phone = arr(4)
+        val name = p_t.findAllIn(arr(2).trim).mkString
+        val raltinfo = ralt.search(cardno).get
+        if(raltinfo != None) {
+          val altbank = raltinfo.the_card_segment
+          val bankcode = raltinfo.bank_code
+          val cardtype = raltinfo.card_type
+          val cardlen = raltinfo.card_len
+          s"[${String.format("%1$-12s", altbank)}|${String.format("%1$-8s", bankcode)}|${String.format("%1$-10s", cardtype)}|${String.format("%1$-12s", cardlen)}]:${String.format("%1$-20s", cardno)}#${name + (" " * (20 - name.length * 2))}#${String.format("%1$-15s", if (phone.length == 11) phone else "")}#${String.format("%1$-20s", license)}#${String.format("%1$-8s", bankcode)}#${String.format("%1$-10s", cardtype)}"
+        }else{
+          ""
+        }
+      })
+      .filter(_.length != 0)
+      .groupBy(_.split("\\|")(0).replaceAll("\\[", "").trim)
+      .map(data => {
+        val alt_bank = data._1
+        val info = data._2.toArray.sortWith(_ > _).take(5)
+        (alt_bank, info)
+      })
+      .sortBy(_._1)
+      .map(data => {
+        data._2.mkString("\n")
+      })
+      .coalesce(10, true)
+      .saveAsTextFile(args(2))
+  }
 }
